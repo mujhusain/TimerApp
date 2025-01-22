6383f52ed7abd03ecb615801b15bb6a80119b86b
Mujaffar Hussain <mujaffarh1@gmail.com>
2025-01-23 00:37:14 +0530

    app name and bundle id changed To timer app

--- android/app/src/main/java/com/timerapp/MainActivity.kt
+++ android/app/src/main/java/com/timerapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.imageapp
+package com.timerapp
 
 import android.os.Bundle;
 
@@ -17,7 +17,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "MyApp"
+  override fun getMainComponentName(): String = "TimerApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/timerapp/MainApplication.kt
+++ android/app/src/main/java/com/timerapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.imageapp
+package com.timerapp
 
 import android.app.Application
 import com.facebook.react.PackageList
